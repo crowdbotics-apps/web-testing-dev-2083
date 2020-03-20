1c6b7beae6e014ad8b0fef00438eb1708530404b
Crowdbotics <team@crowdbotics.com>
2020-03-20 19:21:08 +0000

    Update models

--- users/migrations/0003_user_home.py
+++ users/migrations/0003_user_home.py
@@ -7,14 +7,20 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0005_as_aqa'),
-        ('users', '0002_auto_20200320_1450'),
+        ("home", "0005_as_aqa"),
+        ("users", "0002_auto_20200320_1450"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='home',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='user_home', to='home.HomePage'),
+            model_name="user",
+            name="home",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="user_home",
+                to="home.HomePage",
+            ),
         ),
     ]
--- users/models.py
+++ users/models.py
@@ -9,10 +9,24 @@ class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
     home = models.ForeignKey(
         "home.HomePage",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="user_home",
+    )
+    test = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name="user_home",
+        related_name="user_test",
+    )
+    asdf = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="user_asdf",
     )
 
     def get_absolute_url(self):
